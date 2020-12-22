c978b9f32c0601516965a2f922711dc1cd4a260e
Jules Bonnard <julesbonnard@gmail.com>
2020-12-22 15:51:52 +0100

    Revert "change the favicon"
    This reverts commit 226ff7e9389ed52c71a7cc461c10d43ff069cf2a.

--- vue.config.js
+++ vue.config.js
@@ -25,6 +25,29 @@ module.exports = {
 
   lintOnSave: true,
 
+  pwa: {
+    name: 'AFP Deck',
+    themeColor: '#243447',
+    msTileColor: '#243447',
+    workboxPluginMode: 'InjectManifest',
+    manifestPath: 'manifest.webmanifest',
+    workboxOptions: {
+      swSrc: path.resolve(__dirname, 'src/service-worker.js'),
+      importWorkboxFrom: 'local',
+      exclude: ['robots.txt', /google.*\.html$/, 'CNAME']
+    },
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+    assetsVersion: 1,
+    iconPaths: {
+      favicon32: '/img/icons/32x32.png',
+      favicon16: 'img/icons/16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
+  },
+
   pluginOptions: {
     i18n: {
       localeDir: 'locales'
