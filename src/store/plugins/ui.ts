e755679b39150a3f016e179e32ae7986b5199705
Jules Bonnard <julesbonnard@gmail.com>
2021-06-23 15:36:54 +0200

    update deps

--- src/store/plugins/ui.ts
+++ src/store/plugins/ui.ts
@@ -3,7 +3,7 @@ import { Store } from 'vuex'
 import State from '@/store/state'
 
 export const ui = (store: Store<State>): void => {
-  store.subscribe(async ({ type, payload }: { type: string, payload: any }) => {
+  store.subscribe(async ({ type, payload }: { type: string, payload: unknown }) => {
     let lastColumn
     switch (type) {
       case 'addColumn':
@@ -16,12 +16,14 @@ export const ui = (store: Store<State>): void => {
           })
         }
         break
-      case 'updateColumnDisplayed':
+      case 'updateColumnDisplayed': {
         await Vue.nextTick()
-        if (payload.displayed === true && Date.now() - store.getters.getColumnByIndex(payload.indexCol).lastUpdated > 10000) {
-          store.dispatch('refreshColumn', { indexCol: payload.indexCol, mode: 'after', catchError: true })
+        const params = payload as { displayed: boolean, indexCol: number }
+        if (params.displayed === true && Date.now() - store.getters.getColumnByIndex(params.indexCol).lastUpdated > 10000) {
+          store.dispatch('refreshColumn', { indexCol: params.indexCol, mode: 'after', catchError: true })
         }
         break
+      }
       default:
     }
   })
