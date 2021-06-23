2ec5bf55a3dd5359527a18c70119b3f0c6d4d4ad
Jules Bonnard <julesbonnard@gmail.com>
2021-06-23 17:36:03 +0200

    fix service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,31 +1,40 @@
-workbox.core.setCacheNameDetails({ prefix: 'afpnews-deck' })
+import { registerRoute } from 'workbox-routing'
+import { cacheFirst } from 'workbox-strategies'
+import { precacheAndRoute } from 'workbox-precaching'
+import { setCacheNameDetails } from 'workbox-core'
+import * as googleAnalytics from 'workbox-google-analytics'
+import { ExpirationPlugin } from 'workbox-expiration'
+import { BackgroundSyncPlugin } from 'workbox-background-sync'
+import { CacheableResponsePlugin } from 'workbox-cacheable-response'
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST, {
+setCacheNameDetails({ prefix: 'afpnews-deck' })
+
+precacheAndRoute(self.__WB_MANIFEST, {
   ignoreUrlParametersMatching: [/.*/],
   cleanUrls: false
 })
 
-workbox.routing.registerRoute(
+registerRoute(
   /^https:\/\/(api|afp-apicore-prod)\.afp\.com\/objects\/api\/medias\?id=.*\.jpg$/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'afpnews-assets',
+  cacheFirst({
+    cacheName: 'afpnews-images',
     plugins: [
-      new workbox.expiration.Plugin({
+      new ExpirationPlugin({
         maxEntries: 150,
         maxAgeSeconds: 24 * 60 * 60,
         purgeOnQuotaError: true
       }),
-      new workbox.backgroundSync.Plugin('afpnews-assets-queue', {
+      new BackgroundSyncPlugin('afpnews-images-queue', {
         maxRetentionTime: 5 // Retry for max of 5 minutes
       }),
-      new workbox.cacheableResponse.Plugin({
+      new CacheableResponsePlugin({
         statuses: [0, 200]
       })
     ]
   }), 'GET'
 )
 
-workbox.googleAnalytics.initialize()
+googleAnalytics.initialize()
 
 self.addEventListener('message', event => {
   if (!event.data) {
